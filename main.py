c999972a5f9c39d2f6faa2f14e8f46354bf1a58f
Samuel Flickinger <samuelflickinger@yahoo.com>
2021-12-30 17:34:35 -0500

    Created python package 'onenote_to_md' for execution and updated main.py to be wrapper script to run the 'onenote_to_md' package

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello, OneNote to MD!")
+#!/usr/bin/env python
+# main.py
 
+from onenote_to_md.application import main
 
 if __name__ == "__main__":
+    print("Hello from main.py!")
     main()
